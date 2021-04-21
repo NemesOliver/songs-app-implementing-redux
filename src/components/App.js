38ed3cf243304a785b0b0fe8a299441dfcb8f1fc
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-04-21 15:06:30 +0100

    create reducers and action creators

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
-
+// import { selectSong } from "../actions";
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
